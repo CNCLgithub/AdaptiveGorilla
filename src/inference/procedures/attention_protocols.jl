068933cf57bc6c4974992f18c3a1a23dcf75aa33
mario <mbelledonne@gmail.com>
2024-07-17 10:10:04 -0400

    working on adaptive computation

--- src/inference/procedures/attention_protocols.jl
+++ src/inference/procedures/attention_protocols.jl
@@ -33,3 +33,43 @@ function apply_protocol!(chain::APChain, p::UniformProtocol)
 
     return nothing
 end
+
+export AdaptiveProtocol
+
+@with_kw struct AdaptiveProtocol <: AttentionProtocol
+    """Moves over singles"""
+    single_block! = ReweightBlock(single_ancestral_proposal, 10)
+    """Moves over ensemble"""
+    ensemble_block! = ReweightBlock(ensemble_ancestral_proposal, 3)
+    """Moves over babies"""
+    baby_block! = ReweightBlock(baby_ancestral_proposal, 3)
+end
+
+mutable struct AdaptiveAux <: AuxillaryState
+
+end
+
+function AuxState(::AdaptiveProtocol)
+    AdaptiveAux()
+end
+
+function apply_protocol!(chain::APChain, p::AdaptiveProtocol)
+    @unpack single_block!, ensemble_block!, baby_block! = p
+    @unpack proc, state, auxillary = chain
+    np = length(state.traces)
+    for i = 1:np
+        s = state.traces[i]
+        # 1. Single
+        k = s[:init_state => :k]
+        winner = categorical(Fill(1.0 / k, k))
+        single_block!(state, i, winner)
+
+        # 2. Ensemble
+        bernoulli(0.5) && ensemble_block!(state, i)
+
+        # 3. Baby
+        # bernoulli(0.5) && baby_block!(state, i)
+    end
+
+    return nothing
+end
