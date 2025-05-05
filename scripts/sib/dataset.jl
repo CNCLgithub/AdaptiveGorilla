8172a934b24fc4adc7dbcdc8b808259c60825d2d
mario <mbelledonne@gmail.com>
2025-05-05 08:05:49 -0400

    clean up var names

--- scripts/sib/dataset.jl
+++ scripts/sib/dataset.jl
@@ -6,14 +6,20 @@ using StaticArrays
 const S2V = SVector{2, Float64}
 
 dataset = "sib"
-ntrials = 10
+nscenes = 7 # unique scenes
+# Each scene will have 4 conditions:
+# Gorilla color x parent color
+# light gorilla splitting from target
+# light gorilla splitting from distractor
+# dark gorilla splitting from target
+# dark gorilla splitting from distractor
 fps = 24
 duration = 10 # seconds
 frames = fps * duration
 out_dir = "/spaths/datasets"
 out_path = "$(out_dir)/$(dataset).json"
 
-function gen_trial(wm::SchollWM, nframes::Int64)
+function gen_scene(wm::SchollWM, nframes::Int64)
     _, states = wm_scholl(nframes, wm)
     steps = Vector{Vector{S2V}}(undef,nframes)
     @inbounds for j in 1:nframes
@@ -30,10 +36,11 @@ function gen_trial(wm::SchollWM, nframes::Int64)
     gorilla = Dict(
         # onset time
         :frame =>
-            uniform_discrete(round(Int64, 0.4 * nframes),
-                             round(Int64, 0.6 * nframes)),
+            uniform_discrete(round(Int64, 0.5 * nframes),
+                             round(Int64, 0.8 * nframes)),
+        # random target
         :parent =>
-            uniform_discrete(1, wm.n_dots),
+            uniform_discrete(1, round(Int64, 0.5 * wm.n_dots)),
         # speed to move across
         :speedx => normal(wm.vel, 1.0),
         :speedy => normal(wm.vel, 1.0),
@@ -59,7 +66,7 @@ function main()
         vel_prob = 0.20
     )
     data = Dict()
-    data[:trials] = [gen_trial(wm, frames) for _ = 1:ntrials]
+    data[:trials] = [gen_scene(wm, frames) for _ = 1:nscenes]
     data[:manifest] = Dict(:ntrials => ntrials,
                            :duration => duration,
                            :fps => fps,
