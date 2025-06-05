1c3cc6a4ca4a0057224e71d03be543a56e3b318b
belledon <mbelledonne@gmail.com>
2025-06-05 09:33:11 -0400

    support for ensemble in count estimation

--- src/agent/planning.jl
+++ src/agent/planning.jl
@@ -58,39 +58,45 @@ function plan_with_delta_pi!(
     state = get_last_state(tr)
     @unpack singles, ensembles = state
     ns = length(singles)
-    # ns = min(4, length(singles))
     ne = length(ensembles)
-    ep = 0.0
+    ecount = 0.0
+    ep = -Inf
     @inbounds for j = 1:ns
         dpi = -Inf
         single = singles[j]
         # only consider targets
-        if single.mat == pl.mat
-            # consider each wall
-            # REVIEW: maybe just look at closest wall?
-            for k = 1:4 # each wall
-                (_ep, _dpi) = colprob_and_agrad(single, walls[k])
-                ep += _ep
-                dpi = logsumexp(dpi, _dpi)
+        single.mat != pl.mat && continue
+        # consider each wall
+        # REVIEW: maybe just look at closest wall?
+        for k = 1:4 # each wall
+            (_ep, _dpi) = colprob_and_agrad(single, walls[k])
+            ep = logsumexp(ep, _ep)
+            dpi = logsumexp(dpi, _dpi)
+            if log(rand()) < ep
+                ecount += 1
+                ep = -Inf
             end
+        end
         end
         update_dPi!(att, single, dpi)
     end
     @inbounds for j = 1:ne
         dpi = -Inf
         x = ensembles[j]
-        # # target proportion of ensemble
-        # w = x.matws[Int64(pl.mat)]
-        # for k = 1:4 # each wall
-        #     (_ep, _dpi) = colprob_and_agrad(x, walls[k])
-        #     ep += _ep
-        #     dpi += _dpi
-        # end
-        # ep *= w
-        # dpi *= w
+        # target proportion of ensemble
+        w = x.matws[Int64(pl.mat)]
+        for k = 1:4 # each wall
+            (_ep, _dpi) = colprob_and_agrad(x, walls[k])
+            ep = logsumexp(ep, log(w) + _ep)
+            dpi = logsumexp(dpi, log(w) + _dpi)
+            if log(rand()) < ep
+                ecount += 1
+                ep = -Inf
+            end
+        end
         update_dPi!(att, x, dpi)
     end
-    return ep
+    return ecount
 end
 
 function colprob_and_agrad(pos::S2V, w::Wall)
@@ -103,22 +109,43 @@ function colprob_and_agrad(pos::S2V, w::Wall)
     (p, dpdx)
 end
 
-function colprob_and_agrad(x::InertiaSingle, w::Wall)
-    pos = get_pos(x)
-    d = max(1.0, abs(w.d - sum(w.normal .* pos)) - x.size)
-    p = exp(min(0., -log(d) - 1))
-    dpdx = 1.0 / d
+function colprob_and_agrad(obj::InertiaSingle, w::Wall)
+    x = get_pos(obj)
+    v = get_vel(obj)
+    v_orth = dot(v, w.normal)
+    distance = abs(w.d - dot(x, w.normal))
+    # Distribution over near future
+    var = 0.5 * v_orth
+    mu = 0.5 * v_orth + get_size(obj)
+    pred = Normal(mu, var)
+    # CCDF up to wall
+    p = logccdf(pred, distance)
+    # pdf is the derivative of the cdf
+    dpdx = logpdf(pred, distance)
     (p, dpdx)
 end
 
-function colprob_and_agrad(x::InertiaEnsemble, w::Wall)
-    pos = get_pos(x)
-    d = (w.d - sum(w.normal .* pos))
-    z = d / (sqrt(x.var) * x.rate)
-    p = fast_sigmoid(z) # x, x0, m
-    dpdx = abs(fast_sigmoid_grad(z))
+function colprob_and_agrad(obj::InertiaEnsemble, w::Wall)
+    x = get_pos(obj)
+    vel = get_vel(obj)
+    var = get_var(obj)
+    vel_orth = dot(vel, w.normal)
+    var_orth = dot(var, w.normal)
+    distance = abs(w.d - dot(x, w.normal))
+    # Distribution over near future
+    # Variance integrates ensemble spread
+    # This dilutes probability density
+    var = 0.5 * (v_orth + var_orth)
+    mu = 0.5 * v_orth
+    pred = Normal(mu, var)
+    # CCDF up to wall
+    p = logccdf(pred, distance)
+    # pdf is the derivative of the cdf
+    dpdx = logpdf(pred, distance)
     (p, dpdx)
 end
+
+# VISUALS
 
 using MOTCore: _draw_text
 
