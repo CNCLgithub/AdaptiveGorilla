1c5f462de5f37c6357c6e6b5735dcbb018a06a4e
belledon <mbelledonne@gmail.com>
2025-07-08 09:05:42 -0400

    added script to aggregate batched runs

---
+++ scripts/target_ensemble/aggregate_runs.jl
@@ -0,0 +1,41 @@
+using CSV
+using DataFrames
+using Statistics: mean
+
+
+NOTICE_MIN_FRAMES = 12
+
+DATASET = "target_ensemble/2025-06-09_W96KtK"
+
+MODEL = :full
+BASE_PATH = "/spaths/experiments/$(DATASET)/$(MODEL)"
+RUN_PATH = "$(BASE_PATH)/scenes"
+OUT_PATH = "$(BASE_PATH)/aggregate.csv"
+
+function load_result(path::String)
+    CSV.read(path, DataFrame;
+             header = [:scene, :color, :parent, :chain,
+                       :ndetected, :pct_error],
+             skipto=2) # ignore first row
+end
+
+function merge_results(path::String)
+    files = readdir(path;join = true)
+    filter!(endswith(".csv"), files)
+    all = vcat(map(load_result, files)...)
+    return all
+end
+
+
+function main()
+    all = merge_results(RUN_PATH)
+    g = groupby(all, [:color, :parent])
+    c = combine(g,
+                :ndetected =>
+                    (x -> mean(>(NOTICE_MIN_FRAMES), x)) =>
+                    :noticed)
+    display(c)
+    CSV.write(OUT_PATH, c)
+end
+
+main()
