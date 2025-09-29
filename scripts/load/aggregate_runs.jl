5d74f21ae615239a5647e54634b6028e40aeb94e
belledon <mbelledonne@gmail.com>
2025-09-29 15:22:15 -0400

    Added job files for `load`

--- scripts/load/aggregate_runs.jl
+++ scripts/load/aggregate_runs.jl
@@ -3,14 +3,11 @@ using DataFrames
 using Statistics: mean
 
 
-NOTICE_MIN_FRAMES = 12
+NOTICE_MIN_FRAMES = 18
 
 DATASET = "load_curve"
 
-MODEL = :MO
-BASE_PATH = "/spaths/experiments/$(DATASET)/$(MODEL)-NOTICE"
-RUN_PATH = "$(BASE_PATH)/scenes"
-OUT_PATH = "$(BASE_PATH)/aggregate.csv"
+MODELS = [:MO, :AC, :FR]
 
 function load_result(path::String)
     CSV.read(path, DataFrame)
@@ -23,18 +20,27 @@ function merge_results(path::String)
     return all
 end
 
-
-function main()
+function aggregate_results(model)
+    BASE_PATH = "/spaths/experiments/$(DATASET)/$(model)-NOTICE"
+    RUN_PATH = "$(BASE_PATH)/scenes"
+    OUT_PATH = "$(BASE_PATH)/aggregate.csv"
     all = merge_results(RUN_PATH)
     g = groupby(all, [:scene, :color])
     c = combine(g,
                 :ndetected =>
-                    (x -> mean(>=(NOTICE_MIN_FRAMES), x)) =>
+                    (x -> mean(>(NOTICE_MIN_FRAMES), x)) =>
                     :noticed,
-        :count_error => mean)
-    show(all; allrows=true)
+		:count_error => mean)
     show(c; allrows=true)
     CSV.write(OUT_PATH, c)
+end
+
+function main()
+    for model = MODELS
+        println(model)
+        aggregate_results(model)
+	println("")
+    end
 end
 
 main()
--- scripts/load/run_model.jl
+++ scripts/load/run_model.jl
@@ -111,7 +111,7 @@ VIS_PARTICLE_COUNT = 5
 VIS_HYPER_WINDOW = 12
 
 # Decision-making parameters
-COUNT_COOLDOWN=18 # The minimum time steps (1=~40ms) between collisions
+COUNT_COOLDOWN=8 # The minimum time steps (1=~40ms) between collisions
 
 # Granularity Optimizer; See "?AdaptiveGranularity"
 GO_TAU = 1.0
