5b92b148fce168de2fc730b4fae79f3a60f63cdb
belledon <mbelledonne@gmail.com>
2026-01-25 10:59:53 -0500

    updated job scripts

--- scripts/study1/dsq-jobfile.sh
+++ scripts/study1/dsq-jobfile.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem=4GB --cpus-per-task=8 --partition=day --time=30 --chdir=/gpfs/radev/home/meb266/project/AdaptiveGorilla
 
 # DO NOT EDIT LINE BELOW
-/gpfs/radev/apps/avx512/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/radev/project/yildirim/meb266/AdaptiveGorilla/scripts/most/joblist.txt --status-dir /gpfs/radev/home/meb266/project/AdaptiveGorilla/env.d/spaths/slurm
+/gpfs/radev/apps/avx512/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/radev/project/yildirim/meb266/AdaptiveGorilla/scripts/study1/joblist.txt --status-dir /gpfs/radev/home/meb266/project/AdaptiveGorilla/env.d/spaths/slurm
 
