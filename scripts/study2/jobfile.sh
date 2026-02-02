643dcb6fff4d348e058e1fd3179b1c243bd2310a
belledon <mbelledonne@gmail.com>
2026-02-02 09:09:39 -0500

    updated jobfile

--- scripts/study2/jobfile.sh
+++ scripts/study2/jobfile.sh
@@ -2,7 +2,7 @@
 #SBATCH --output /gpfs/radev/home/meb266/project/AdaptiveGorilla/env.d/spaths/slurm/%A_%a.out
 #SBATCH --array 0-23
 #SBATCH --job-name gorillas
-#SBATCH --mem=4GB --cpus-per-task=8 --partition=day --time=30 --chdir=/gpfs/radev/home/meb266/project/AdaptiveGorilla
+#SBATCH --mem=4GB --cpus-per-task=4 --partition=day --time=45 --chdir=/gpfs/radev/home/meb266/project/AdaptiveGorilla
 
 # DO NOT EDIT LINE BELOW
 /gpfs/radev/apps/avx512/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/radev/project/yildirim/meb266/AdaptiveGorilla/scripts/study2/joblist.txt --status-dir /gpfs/radev/home/meb266/project/AdaptiveGorilla/env.d/spaths/slurm
